a8a235f66302eed8547515a36784fd8f9245f86d
hugosenari <hugosenari@gmail.com>
2022-03-27 18:14:50 -0300

    feat(nimpkgs): remove lock

--- main/flake.nix
+++ main/flake.nix
@@ -11,6 +11,7 @@
   inputs.src-dnsprotocol-main.ref   = "refs/heads/main";
   inputs.src-dnsprotocol-main.owner = "rockcavera";
   inputs.src-dnsprotocol-main.repo  = "nim-dnsprotocol";
+  inputs.src-dnsprotocol-main.dir   = "";
   inputs.src-dnsprotocol-main.type  = "github";
   
   inputs."stew".owner = "nim-nix-pkgs";
